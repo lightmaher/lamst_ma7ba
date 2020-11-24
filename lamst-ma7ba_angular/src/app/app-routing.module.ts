a9725a52a0205c72d4c906602425c5fa70e6a915
lightmaher <lightmaher3@gmail.com>
2020-11-24 22:55:52 +0200

    detail

--- lamst-ma7ba_angular/src/app/app-routing.module.ts
+++ lamst-ma7ba_angular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { AddEventComponent } from './events/add-event/add-event.component';
+import { EventDetailsComponent } from './events/event/event-details/event-details.component';
 import { EventsComponent } from './events/events.component';
 import { HomeComponent } from './home/home.component';
 import { PlaceAddComponent } from './places/place-add/place-add.component';
@@ -17,7 +18,8 @@ const routes: Routes = [
   {path: 'add-place' , component: PlaceAddComponent},
   {path: 'edit-place/:id' , component: PlaceAddComponent},
   {path: 'place-detail/:id' , component: PlaceDetailComponent},
-  {path: 'login' , component: AccountComponent}
+  {path: 'login' , component: AccountComponent},
+  {path: 'event-detail/:id' , component: EventDetailsComponent}
 ];
 
 @NgModule({
