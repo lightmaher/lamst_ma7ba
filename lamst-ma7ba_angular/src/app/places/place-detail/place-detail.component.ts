6daf435c1898937d5d32b7258e586c2973e6abc2
lightmaher <lightmaher3@gmail.com>
2020-11-25 19:14:23 +0200

    x

--- lamst-ma7ba_angular/src/app/places/place-detail/place-detail.component.ts
+++ lamst-ma7ba_angular/src/app/places/place-detail/place-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PlaceService } from '../../_Services/place.service';
 import { Place } from '../../_Models/place';
+import { EventService } from 'src/app/_Services/event.service';
+import { Evt } from 'src/app/_Models/Evt';
 
 @Component({
   selector: 'app-place-detail',
@@ -11,8 +13,13 @@ import { Place } from '../../_Models/place';
 export class PlaceDetailComponent implements OnInit {
 
   place: Place;
+  evet:Evt[];
+  evt: Evt;
+  nid:number;
+  fevet: Evt[];
   constructor(private placeS: PlaceService ,
               private activeRoute: ActivatedRoute ,
+              private eventsservice: EventService,
               private route: Router) { }
 
   ngOnInit(): void {
@@ -20,6 +27,8 @@ export class PlaceDetailComponent implements OnInit {
     this.placeS.GetPlace(id).subscribe(x => {
       this.place = x;
     } , err => console.log(err));
+    this.showevents();
+
   }
 
   public createImgPath = (server: string) => {
@@ -38,4 +47,13 @@ export class PlaceDetailComponent implements OnInit {
       } , err => console.log(err));
     }
   }
+  showevents(){
+    this.eventsservice.getAllEvents().subscribe(
+      (events: Evt[]) => {this.evet= events;
+        this.nid = this.place.placeId
+        this.fevet = (this.nid)?
+        this.evet.filter(p => p.location.placeId === this.nid):this.evet;
+      }
+    );
+    }
 }
